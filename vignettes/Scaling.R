0d9a63f267b230ad6013bdafcd40fc979961e972
David Smith <smitdave@gmail.com>
2024-11-21 08:56:49 -0800

    towards evaluation

--- vignettes/Scaling.R
+++ vignettes/Scaling.R
@@ -4,11 +4,11 @@ library(ramp.work)
 library(deSolve)
 
 ## ----echo=F-------------------------------------------------------------------
-#devtools::load_all()
+devtools::load_all()
 
 ## ----Fsin---------------------------------------------------------------------
 tt <- seq(0, 730, by=5) 
-p1 <- makepar_F_sin(floor=0.1)
+p1 <- makepar_F_sin(floor=0.2, pw=2)
 Fsin <- make_function(p1)
 plot(tt, Fsin(tt), type="l")
 
@@ -29,15 +29,7 @@ require(viridis)
 clrs = turbo(25)
 
 ## ----fig.height=3.5, fig.width=5----------------------------------------------
-plot_eirpr(sis)
-
-with(sis$output$eirpr,{
-  points(aeir, pr, col = clrs)
-  lines(scaling[[5]]$aeir, scaling[[5]]$pr, col = clrs[5])
-  lines(scaling[[10]]$aeir, scaling[[10]]$pr, col = clrs[10])
-  lines(scaling[[15]]$aeir, scaling[[15]]$pr, col = clrs[15])
-  lines(scaling[[20]]$aeir, scaling[[20]]$pr, col = clrs[20])
-})
+eirpr_seasonal_profile(c(5,10,15,20), sis, clrs)
 
 ## -----------------------------------------------------------------------------
 preir_i = xde_pr2eir(c(0.001, runif(25, 0, 1), 0.999), sis)
