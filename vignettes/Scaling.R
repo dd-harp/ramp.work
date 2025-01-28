a773d1663cc270a6f07e0be90b517fa1b90b9607
David Smith <smitdave@gmail.com>
2025-01-28 00:18:24 -0800

    temporary

--- vignettes/Scaling.R
+++ vignettes/Scaling.R
@@ -2,15 +2,16 @@
 library(ramp.xds)
 library(ramp.work)
 library(deSolve)
+library(viridisLite)
 
 ## ----echo=F-------------------------------------------------------------------
 devtools::load_all()
 
-## ----Fsin---------------------------------------------------------------------
+## ----Fsin, fig.height=4, fig.width=7------------------------------------------
 tt <- seq(0, 730, by=5) 
-p1 <- makepar_F_sin(floor=0.2, pw=2)
+p1 <- makepar_F_sin(floor=0.2, pw=1.2)
 Fsin <- make_function(p1)
-plot(tt, Fsin(tt), type="l")
+plot(tt, Fsin(tt), type="l", xlab = "Time (Days)", ylab = "Seasonal Pattern")
 
 ## -----------------------------------------------------------------------------
 xds_setup_cohort(Xname = "SIS", F_season=Fsin) -> sis
@@ -21,15 +22,27 @@ xds_solve_cohort(sis) -> sis
 ## -----------------------------------------------------------------------------
 xde_scaling_eir(sis, 25) -> sis
 
-## -----------------------------------------------------------------------------
+## ----fig.height=4, fig.width=7------------------------------------------------
 plot_eirpr(sis)
 
-## -----------------------------------------------------------------------------
-require(viridis)
-clrs = turbo(25)
+## ----fig.height=4, fig.width=7------------------------------------------------
+eirpr_seasonal_profile(c(5,10,15,20), sis, clrs = turbo(25))
 
-## ----fig.height=3.5, fig.width=5----------------------------------------------
-eirpr_seasonal_profile(c(5,10,15,20), sis, clrs)
+## -----------------------------------------------------------------------------
+Lo = list(
+  F_season = Fsin
+)
+xds_setup(Xname = "SIS", Lopts = Lo) -> sis_full
+
+## -----------------------------------------------------------------------------
+xds_solve(sis_full) -> sis_full
+
+## -----------------------------------------------------------------------------
+devtools::load_all()
+xde_scaling_lambda(sis_full, 1.1, 30) -> sis_full
+
+## -----------------------------------------------------------------------------
+plot_eirpr(sis_full)
 
 ## -----------------------------------------------------------------------------
 preir_i = xde_pr2eir(c(0.001, runif(25, 0, 1), 0.999), sis)
